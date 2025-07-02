0ec58f5c23345ae130bf910bd8e9184411b121ae
Gabriel Souza <souzagabriels@hotmail.com>
2025-07-02 00:50:57 -0300

    :sparkles: Adicionando componente navbar e footer e atualizando a home

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,30 @@
+function Navbar() {
+  return (
+    <>
+      <div className="w-full flex justify-center py-4 bg-white text-purple-900">
+        <div className="w-full flex justify-between text-lg px-4">
+          Blog Pessoal
+          <ul className="flex gap-4 font-bold">
+            <li className="hover:text-purple-700 transition-colors cursor-pointer px-2 py-1 rounded hover:bg-purple-100">
+              Postagens
+            </li>
+            <li className="hover:text-purple-700 transition-colors cursor-pointer px-2 py-1 rounded hover:bg-purple-100">
+              Temas
+            </li>
+            <li className="hover:text-purple-700 transition-colors cursor-pointer px-2 py-1 rounded hover:bg-purple-100">
+              Cadastrar tema
+            </li>
+            <li className="hover:text-purple-700 transition-colors cursor-pointer px-2 py-1 rounded hover:bg-purple-100">
+              Perfil
+            </li>
+            <li className="hover:text-red-600 transition-colors cursor-pointer px-2 py-1 rounded hover:bg-red-100">
+              Sair
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
