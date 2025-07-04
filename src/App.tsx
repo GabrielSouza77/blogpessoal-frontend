3ebd2f09e1b4177a8e8f173b478167be9f4fe51c
Gabriel Souza <souzagabriels@hotmail.com>
2025-07-04 03:57:32 -0300

    :sparkles: Adicionando telas de cadastro e login funcionais

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,6 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Cadastro from "./pages/cadastro/Cadastro";
 import { AuthContext } from "./contexts/AuthContext";
-// import ListaTemas from "./components/temas/listatemas/ListaTemas";
-// import FormTema from "./components/temas/formtema/FormTema";
-// import DeletarTema from "./components/temas/deletartema/DeletarTema";
-// import ListaPostagens from "./components/postagens/listapostagens/ListaPostagens";
-// import FormPostagem from "./components/postagens/formpostagem/FormPostagem";
-// import DeletarPostagem from "./components/postagens/deletarpostagem/DeletarPostagem";
-// import Perfil from "./pages/perfil/Perfil";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
 
 function requireAuth(element: React.ReactElement, token: string) {
   return token ? element : <Login />;
