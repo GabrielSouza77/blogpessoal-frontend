6876c9e62854bb0193cd53c7a6313d565fa6a5ee
Gabriel Souza <souzagabriels@hotmail.com>
2025-07-11 01:21:49 -0300

    :fix: Ajustando para produção

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,6 @@
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string;
-}
+/// <reference types="vite/client" />
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
+declare module "*.css" {
+  const content: string;
+  export default content;
 }
